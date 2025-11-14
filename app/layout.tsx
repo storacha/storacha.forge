39736371ca84c06fe3340f5bc2f43362cbc6e226
Dhruv Varshney <dhruvvarshney77@gmail.com>
2025-11-14 22:18:36 +0530

    Add props

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,46 +1,56 @@
-import type { Metadata } from 'next'
-import { Epilogue, DM_Sans } from 'next/font/google'
-import './globals.css'
-import PlausibleProvider from 'next-plausible';
+import type { Metadata } from "next";
+import { Epilogue, DM_Sans } from "next/font/google";
+import "./globals.css";
+import PlausibleProvider from "next-plausible";
 
 const epilogue = Epilogue({
-  variable: '--font-epilogue',
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  display: 'swap',
-})
+  variable: "--font-epilogue",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+});
 
 const dmSans = DM_Sans({
-  variable: '--font-dm-sans',
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700', '800', '900'],
-  display: 'swap',
-})
+  variable: "--font-dm-sans",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
-  title: 'Storacha Forge - Cut Cloud Backup Costs by 80%',
-  description: 'Decentralized backup & archival for enterprises, secure, verifiable, zero lock-in. Your data, governed by protocol, not corporations.',
-  keywords: 'cloud storage, decentralized storage, backup solutions, enterprise storage, filecoin, IPFS, data ownership',
+  title: "Storacha Forge - Cut Cloud Backup Costs by 80%",
+  description:
+    "Decentralized backup & archival for enterprises, secure, verifiable, zero lock-in. Your data, governed by protocol, not corporations.",
+  keywords:
+    "cloud storage, decentralized storage, backup solutions, enterprise storage, filecoin, IPFS, data ownership",
   openGraph: {
-    title: 'Storacha Forge - Cut Cloud Backup Costs by 80%',
-    description: 'Decentralized backup & archival for enterprises, secure, verifiable, zero lock-in.',
-    images: ['/forge/hero/earth-bg.png'],
-    type: 'website',
+    title: "Storacha Forge - Cut Cloud Backup Costs by 80%",
+    description:
+      "Decentralized backup & archival for enterprises, secure, verifiable, zero lock-in.",
+    images: ["/forge/hero/earth-bg.png"],
+    type: "website",
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" className={`${epilogue.variable} ${dmSans.variable}`}>
       <body className="font-sans antialiased">
-      <PlausibleProvider domain="forge.staging.storacha.network">
-        {children}
+        <PlausibleProvider
+          domain="forge.staging.storacha.network"
+          trackFileDownloads={true}
+          trackOutboundLinks={true}
+          taggedEvents={true}
+          trackLocalhost={true}
+          enabled={true}
+        >
+          {children}
         </PlausibleProvider>
       </body>
     </html>
-  )
+  );
 }
